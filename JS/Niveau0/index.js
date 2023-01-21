49791e88440f6ee81bc0cacdc70bcab2c8719d26
TheFish <demetlegal@gmail.com>
2023-01-21 01:18:14 +0100

    Update index.js

--- JS/Niveau0/index.js
+++ JS/Niveau0/index.js
@@ -1,5 +1,11 @@
 function boutton1() {
-    const titre = document.getElementById("titre");
+    const titre = document.getElementById("titre"); // Cherche l'élément titre
 
-    titre.innerHTML = "Boutton 1";
+    titre.innerHTML = "Boutton 1"; // Modifie le texte du titre
+}
+
+const boutton2 = document.getElementById("boutton2"); // Cherche l'élément boutton
+boutton2.onclick = () => { // Voici la syntaxe de la fonction arrow
+    const titre = document.getElementById("titre");
+    titre.innerHTML = "Boutton 2";
 }
